00f216492889713cefe42946d38a42fc71cc43e8
Yury Bandarchuk <yury.bandarchuk@gmail.com>
2017-11-01 00:43:19 +0300

    Basic UI designed

---
+++ Pods/Target Support Files/Pods-MyMood/Pods-MyMood-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyMood : NSObject
+@end
+@implementation PodsDummy_Pods_MyMood
+@end
---
+++ Pods/Target Support Files/Pods-MyMoodTests/Pods-MyMoodTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyMoodTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyMoodTests
+@end
