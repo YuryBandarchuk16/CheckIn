63247d57c1b6e7d03ec83e0428d57ca3036d02f2
Yury Bandarchuk <yury.bandarchuk@gmail.com>
2017-11-05 13:16:03 +0300

    Write It view added and lots of bugs fixed

--- MyMood/Student/StudentMainViewController.swift
+++ MyMood/Student/StudentMainViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 import Firebase
 import Foundation
 
-class StudentMainViewController: UIViewController {
+class StudentMainViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var dateLabel: UILabel!
+    
+    private var keyboardAdjusted = false
+    private var visibleLocation: CGFloat!
+    private var lastKeyboardOffset: CGFloat = 0.0
+    private let animationDuration: TimeInterval = 0.5
+    
+    @IBOutlet weak var teacherCheckInSwitch: UISwitch!
     
     private var currentButtonMask: Int = 0
     
@@ -25,19 +32,33 @@ class StudentMainViewController: UIViewController {
     @IBOutlet weak var confusedButton: UIButton! //6th
     @IBOutlet weak var worriedButton: UIButton! // 7th
     
+    @IBOutlet weak var writeItTextView: UITextView!
+    
     private var checked: UIImage!
     private var unchecked: UIImage!
     
+    @IBOutlet weak var feelingWordView: UIView!
+    @IBOutlet weak var writeItView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checked = UIImage(named: "checkbox")!
         unchecked = UIImage(named: "checkboxbasic")!
+        writeItTextView.delegate = self
+        visibleLocation = teacherCheckInSwitch.frame.origin.y
+        setupViews()
+    }
+    
+    private func setupViews() {
+        feelingWordView.isHidden = false
+        writeItView.isHidden = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         displayDate()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
     
     private func displayDate() {
@@ -49,7 +70,25 @@ class StudentMainViewController: UIViewController {
         let year = components.year!
         dateLabel.text = "\(day) \(monthName) \(year)"
     }
-
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textView.resignFirstResponder()
+    }
+    
+    @IBAction func segmentControlValueChanged(_ sender: UISegmentedControl) {
+        self.view.endEditing(true)
+        if (sender.selectedSegmentIndex == 0) {
+            writeItView.isHidden = true
+            feelingWordView.isHidden = false
+        } else if (sender.selectedSegmentIndex == 2) {
+            writeItView.isHidden = false
+            feelingWordView.isHidden = false
+        } else {
+            print("Unsupported for now")
+        }
+    }
+    
+    
     @IBAction func feelingWordCheckBoxClicked(_ sender: UIButton) {
         var buttonId: Int = 0
         switch sender {
@@ -71,7 +110,44 @@ class StudentMainViewController: UIViewController {
         if currentImage == 0 {
             sender.setImage(checked, for: .normal)
         } else {
-            sender.setImage(checked, for: .normal)
+            sender.setImage(unchecked, for: .normal)
         }
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        if keyboardAdjusted == false {
+            lastKeyboardOffset = getKeyboardHeight(notification: notification)
+            UIView.animate(withDuration: animationDuration, animations: {
+                self.view.frame.origin.y -= self.lastKeyboardOffset
+            })
+            keyboardAdjusted = true
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        if keyboardAdjusted == true {
+            UIView.animate(withDuration: animationDuration, animations: {
+                self.view.frame.origin.y += self.lastKeyboardOffset
+            })
+            keyboardAdjusted = false
+        }
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        let needHeight = visibleLocation + keyboardSize.cgRectValue.height
+        let result = max(0, needHeight - UIScreen.main.bounds.height + 30)
+        return result
+    }
+    
 }
